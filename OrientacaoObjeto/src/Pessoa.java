7c010365a09bb8c59f4d147e861fc9d54cec2c5f
ka1quegs <102962617+ka1quegs@users.noreply.github.com>
2022-04-18 08:55:52 -0300

    Add files via upload

--- OrientacaoObjeto/src/Pessoa.java
+++ OrientacaoObjeto/src/Pessoa.java
@@ -1,13 +1,28 @@
-
 public class Pessoa {
 	String nome;
 	int numFigurinhas;
 	
-	void receber(int numFigurinhas) {
+	void receber (int numFigurinhas) {
 		this.numFigurinhas += numFigurinhas;
+				
 	}
 	
-	void dar() {
+	boolean dar(int numFigurinhas, Pessoa pessoa) {
 		
+		if (this.numFigurinhas<numFigurinhas) {
+		System.out.println("Você não tem o numero de figurinhas suficiente para realizar esta ação ");
+	
+		
+		return false; 
+			
 	}
+		
+		else {
+			this.numFigurinhas -= numFigurinhas;
+			pessoa.receber(numFigurinhas);
+			
+			return true;
+		}
+		}
+		
 }
--- OrientacaoObjeto/src/main.java
+++ OrientacaoObjeto/src/main.java
@@ -2,15 +2,37 @@
 public class main {
 
 	public static void main(String[] args) {
+
 		Pessoa pessoa1 = new Pessoa();
-		
+
 		pessoa1.nome = "Kaique";
 		pessoa1.numFigurinhas = 1;
-		
+
 		pessoa1.receber(3);
-		
 		System.out.println(pessoa1.nome);
 		System.out.println(pessoa1.numFigurinhas);
-	}
 
+		Pessoa pessoa2 = new Pessoa();
+		pessoa2.nome = "José";
+		pessoa2.numFigurinhas = 3;
+
+		System.out.println(pessoa2.nome);
+		System.out.println(pessoa2.numFigurinhas);
+
+		boolean retorno = pessoa1.dar(5, pessoa2);
+
+		if (retorno == true) {
+
+			pessoa1.dar(5, pessoa2);
+
+			System.out.println("depois");
+
+			System.out.println(pessoa1.nome);
+			System.out.println(pessoa1.numFigurinhas);
+
+			System.out.println(pessoa2.nome);
+			System.out.println(pessoa2.numFigurinhas);
+
+		}
+	}
 }
