986352b97bb54719e668f59322f9ffb39466b39b
ka1quegs <102962617+ka1quegs@users.noreply.github.com>
2022-05-09 11:30:52 -0300

    Add files via upload

--- Polimorfismo/src/Cachorro.java
+++ Polimorfismo/src/Cachorro.java
@@ -1,9 +1,12 @@
-
 public class Cachorro extends Animal {
 
 	@Override
 	public void Falar() {
 		System.out.println("AUAUAUAUAUAUAUAUAU");
 	}
-	
+	public void Morder() {
+		System.out.println("CRACK!!!!!!!!!!!!!!!!!!!!");
+	}
 }
+
+
--- Polimorfismo/src/Main.java
+++ Polimorfismo/src/Main.java
@@ -3,16 +3,30 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		Animal animal = new Cachorro();
-		System.out.println("Cachorro falou:");
-		animal.Falar();
-		
-		System.out.println();
-		
-		animal = new Gato();
-		System.out.println("E o gato respondeu: ");
-		animal.Falar();
+//		Animal animal = new Cachorro();
+//		System.out.println("Cachorro falou:");
+//		animal.Falar();
+//		
+//		System.out.println();
+//		
+//		animal = new Gato();
+//		System.out.println("E o gato respondeu: ");
+//		animal.Falar();
 
+		Gato gato = new Gato();
+			Cachorro cachorro = new Cachorro();
+			falar(gato);
+			falar(cachorro);
+		
 	}
-
+	public static void falar(Animal a) {
+		a.Falar();
+		
+		if(a instanceof Cachorro) {
+			Cachorro c = (Cachorro) a;
+			c.Morder();
+		}
+	}
+	
 }
+
