b80bd360f01635db6db1c8d887b608f355232a28
ka1quegs <102962617+ka1quegs@users.noreply.github.com>
2022-05-15 17:11:01 -0300

    Update Item.java

--- ClassesAbstratas/src/Item.java
+++ ClassesAbstratas/src/Item.java
@@ -1,23 +1,23 @@
 
 public abstract class Item {	//Superclass
-	private int posX;
+	private int posX;    //criaçao de variavel
 	private int posY;
 	
-	public abstract void pegar() ;
+	public abstract void pegar() ; //criaçao do metodo pegar
 		
-	public int getPosX() {
+	public int getPosX() {  //pega o valor da PosX e retorna o mesmo
 		return posX;
 	}
 
-	public void setPosX(int posX) {
+	public void setPosX(int posX) {  //Modifica o valor da PosX dentro de uma nova variavel que pode ser usada apenas nesse bloco de código
 		this.posX = posX;
 	}
 
-	public int getPosY() {
+	public int getPosY() { //pega o valor da PosY e retorna o mesmo
 		return posY;
 	}
 
-	public void setPosY(int posY) {
-		this.posY = posY;
+	public void setPosY(int posY) { 	//Modifica o valor da PosY dentro de uma nova variavel que pode ser usada apenas nesse bloco de código
+		this.posY = posY; 
 }
 }
