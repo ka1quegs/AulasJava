47f88644a9ed8c9b1f4322b58671a29926486feb
ka1quegs <102962617+ka1quegs@users.noreply.github.com>
2022-05-15 17:28:36 -0300

    Update Matematica.java

--- ElementosStaticos/src/Matematica.java
+++ ElementosStaticos/src/Matematica.java
@@ -1,12 +1,12 @@
 
 public class Matematica {
 	
-	public static int somar(int a, int b) {
+	public static int somar(int a, int b) { //criaçao de metodos com parametros de dois valores EX: (1 , 2) retornará o valor 3
 	return a + b;
 	
 }
 	
-	public static int subtrair(int a, int b) {
+	public static int subtrair(int a, int b) { //criaçao de metodos com parametros de dois valores EX: (1 , 2) retornará o valor -1
 		return a - b;
 		
 	}
