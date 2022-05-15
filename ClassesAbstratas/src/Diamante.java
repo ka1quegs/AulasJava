917c3366e540cc489d6cb55184a595f921d09d47
ka1quegs <102962617+ka1quegs@users.noreply.github.com>
2022-05-15 17:07:49 -0300

    Update Diamante.java

--- ClassesAbstratas/src/Diamante.java
+++ ClassesAbstratas/src/Diamante.java
@@ -2,7 +2,7 @@
 public class Diamante extends Item {  //DIamante herda de Item
 
 	@Override
-	public void pegar() {
+	public void pegar() {      ////Subscreve o metodo pegar com mensagem para tal item
 		System.out.println("Pegou diamante");
 	}
 
