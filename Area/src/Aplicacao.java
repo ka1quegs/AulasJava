0b4543ff3a871041898666a4df028d38311616e7
ka1quegs <102962617+ka1quegs@users.noreply.github.com>
2022-05-15 16:57:30 -0300

    Update Aplicacao.java

--- Area/src/Aplicacao.java
+++ Area/src/Aplicacao.java
@@ -3,18 +3,18 @@ public class Aplicacao {
 
 	public static void main(String[] args) {
 
-		Quadrado quadrado = new Quadrado();
+		Quadrado quadrado = new Quadrado(); //instanciaçao de objeto
 		quadrado.setLado(100);
-		quadrado.calcularArea();
+		quadrado.calcularArea(); //usado o metodo para tal objeto
 		
 		
 		
-		Triangulo triangulo = new Triangulo();
+		Triangulo triangulo = new Triangulo(); //instanciaçao de objeto
 
-		triangulo.setBase(50);;
-		triangulo.setAltura(10);
+		triangulo.setBase(50);; //dado o valor da base
+		triangulo.setAltura(10); //dado o valor da altura
 		
-		triangulo.calcularArea();
+		triangulo.calcularArea();  //usado o metodo para tal objeto
 	}
 
 }
