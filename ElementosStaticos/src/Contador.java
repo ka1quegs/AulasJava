affbdc49b263d5296df58665f3a95ae8de7c73e2
ka1quegs <102962617+ka1quegs@users.noreply.github.com>
2022-05-15 17:26:56 -0300

    Update Contador.java

--- ElementosStaticos/src/Contador.java
+++ ElementosStaticos/src/Contador.java
@@ -1,28 +1,28 @@
 
 public class Contador {
 
-	private static int valor;
+	private static int valor; //criacao de uma variavel do tipo inteiro (int)
 	
 	
 	
-//	public Contador() {
+//	public Contador() {  
 //		this.valor = 1;
 //	}
-	static{
+	static{  //informaçao fica estatica, e dá para ser usado para implementaçao de valores futuros
 		valor = 1;
 	}
 	
 
-	public static void incrementar() {
+	public static void incrementar() {  //incrementa um valor na variavel
 		valor ++;
 	}
 	
 	
-	public static int getValor() {
+	public static int getValor() { //pega o valor da variavel e retorna ele mesmo
 		return valor;
 	}
 
-	public void setValor(int valor) {
+	public void setValor(int valor) {  //modifica o valor da variavel,dentro de uma nova variavel que poderá ser usada apenas nesse bloco de codigo
 		this.valor = valor;
 	}
 	
