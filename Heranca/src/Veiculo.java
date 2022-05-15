cf34361c8751150b50de16e32d23f9f4fabe757c
ka1quegs <102962617+ka1quegs@users.noreply.github.com>
2022-05-15 17:36:25 -0300

    Update Veiculo.java

--- Heranca/src/Veiculo.java
+++ Heranca/src/Veiculo.java
@@ -1,51 +1,51 @@
 package src;
 
 public class Veiculo {
-	
-	private String nome;
+	 
+	private String nome;     //variaveis do veiculo
 	protected String marca;
 	protected String modelo;
 	protected int ano;
 	
-	public void buzinar() {
+	public void buzinar() {     //criacao de metodo
 		System.out.println(("BI BI"));
 	}
 	
-	public void imprimir() {
+	public void imprimir() {   //criacao de metodo
 		System.out.println("Marca: " + marca);
 		System.out.println("Modelo " + modelo);
 		System.out.println("Ano: " + ano);
 	}
 
-	public String getNome() {
+	public String getNome() {  //recebe o valor e retorna ele mesmo
 		return nome;
 	}
 
-	public void setNome(String nome) {
+	public void setNome(String nome) {  //modifica o valor da variavel e coloca em uma nova variavel instanciada
 		this.nome = nome;
 	}
 
-	public String getMarca() {
+	public String getMarca() { //recebe o valor e retorna ele mesmo
 		return marca;
 	}
 
-	public void setMarca(String marca) {
+	public void setMarca(String marca) {  //modifica o valor da variavel e coloca em uma nova variavel instanciada
 		this.marca = marca;
 	}
 
-	public String getModelo() {
+	public String getModelo() {  //recebe o valor e retorna ele mesmo
 		return modelo;
 	}
 
-	public void setModelo(String modelo) {
-		this.modelo = modelo;
+	public void setModelo(String modelo) {   //modifica o valor da variavel e coloca em uma nova variavel instanciada
+		this.modelo = modelo; 
 	}
 
-	public int getAno() {
+	public int getAno() {  //recebe o valor e retorna ele mesmo
 		return ano;
 	}
 
-	public void setAno(int ano) {
+	public void setAno(int ano) {  //modifica o valor da variavel e coloca em uma nova variavel instanciada
 		this.ano = ano;
 	}
 	
