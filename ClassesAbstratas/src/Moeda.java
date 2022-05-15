afc045a2e1f9c86c7e158e2a462cb9df6964c585
ka1quegs <102962617+ka1quegs@users.noreply.github.com>
2022-05-15 17:11:36 -0300

    Update Moeda.java

--- ClassesAbstratas/src/Moeda.java
+++ ClassesAbstratas/src/Moeda.java
@@ -2,7 +2,7 @@
 public class Moeda extends Item { // Moeda herda de Item
 
 	@Override
-	public void pegar() {
+	public void pegar() {    //subescreve o metodo pegar, e coloca uma mensagem para tal item
 		System.out.println("Pegou moeda");
 	}
 
