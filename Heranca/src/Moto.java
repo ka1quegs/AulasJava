91e9691cb9c4e512b0ac84ff48b90b9973c998e0
ka1quegs <102962617+ka1quegs@users.noreply.github.com>
2022-05-15 17:33:56 -0300

    Update Moto.java

--- Heranca/src/Moto.java
+++ Heranca/src/Moto.java
@@ -1,13 +1,13 @@
 package src;
 
-public class Moto extends Veiculo {
+public class Moto extends Veiculo {  //metodo exclusivo para esse tipo de veiculo
 	public void empinar() {
 		System.out.println("Empinando: BOLOLOLOLOLOLO");
 		
 	}
 
 	@Override
-	public void buzinar() {
+	public void buzinar() {  //Metodo modificado para apenas esse objeto
 		// TODO Auto-generated method stub
 		System.out.println("PIIIIIIIIIIIIIIIIIIIIIIIIIII");
 	}
