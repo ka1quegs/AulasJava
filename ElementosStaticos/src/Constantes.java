d7a5fb0e1786579d99794dfc7f4d264b2b4452d5
ka1quegs <102962617+ka1quegs@users.noreply.github.com>
2022-05-15 17:30:03 -0300

    Update Constantes.java

--- ElementosStaticos/src/Constantes.java
+++ ElementosStaticos/src/Constantes.java
@@ -2,9 +2,9 @@
 public class Constantes {
 
 //	public	static final double MEDIA_DA_PROVA = 7.0;
-	public	static final double MEDIA_DA_PROVA;
+	public	static final double MEDIA_DA_PROVA; //criaçao de uma variavel do tipo double e estatica
 	static {
-		MEDIA_DA_PROVA = 7.0;	
+		MEDIA_DA_PROVA = 7.0;	 //recebe um valor estatico(fixo)
 	}
 	
 	
