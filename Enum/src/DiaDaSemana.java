582ac794eba1ee9e2ecbd34e40a69e1f4cf2b0e3
ka1quegs <102962617+ka1quegs@users.noreply.github.com>
2022-06-09 00:54:29 -0300

    Update DiaDaSemana.java

--- Enum/src/DiaDaSemana.java
+++ Enum/src/DiaDaSemana.java
@@ -1,4 +1,25 @@
+public enum DiaDaSemana {
+	
+	//Dias da semana (Valor)
+	DOMINGO(1),
+	SEGUNDA(2),
+	TERCA(3),
+	QUARTA(4),
+	QUINTA(5),
+	SEXTA(6),
+	SABADO(7);
+	
+	
+	private int num;
 
-public class DiaDaSemana {
 
+	private DiaDaSemana(int num) {
+		this.num = num;
+	}
+
+	public int getNum() {
+		return num;
+	}
+
+	
 }
